e25eccce2815b805be5adaa812c149e677921a73
Alireza Bahari <mralirezabahari@gmail.com>
2022-08-24 16:35:40 +0430

    Connect To Posgresql

--- Bot/handlers/models.go
+++ Bot/handlers/models.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	"gorm.io/gorm"
@@ -14,14 +15,25 @@ type Handler struct {
 }
 
 type UserRedis struct {
-	UserID   int64 `json:"user_id"`
-	IsLock   bool  `json:"lock"`
-	TimeSpam int64 `json:"time_spam"`
-	Warn     byte  `json:"warn"`
+	UserID   int64  `json:"user_id"`
+	IsLock   bool   `json:"lock"`
+	TimeSpam int64  `json:"time_spam"`
+	Warn     byte   `json:"warn"`
+	Location string `json:"loc"`
 }
 
 type User struct {
-	UserID  int64   `json:"user_id" gorm:"primaryKey"`
-	Balance float32 `json:"balance"`
-	Wallet  string  `json:"wallet_address"`
+	UserID        int64   `json:"user_id" gorm:"primaryKey"`
+	Balance       float32 `json:"balance"`
+	Wallet        string  `json:"wallet_address"`
+	DepositAddres string  `json:"deposit_address"`
+	Payment       Payment `json:"payments" gorm:"references:UserID"`
+}
+type Payment struct {
+	PID    int32 `json:"pyment_id" gorm:"primaryKey"`
+	UserID int64
+	TxID   string    `json:"tx_id"`
+	Type   string    `json:"type"`
+	Amount float32   `json:"amount"`
+	Date   time.Time `json:"time"`
 }
